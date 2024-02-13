514a1b17331fc6eecc365e37661d01f1902ea395
Nikita Dubyk <nikitad@guardianvets.com>
2024-02-13 11:53:17 +0300

    fix github auth, add get session

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import { Inter } from "next/font/google";
+import { getServerSession } from "next-auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession({ secret: process.env.AUTH_SECRET });
+  console.log("SESSION", session);
   return <main>Hello!</main>;
 }
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,8 +6,9 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 const prisma = new PrismaClient();
 
 export default NextAuth({
-  session: { strategy: "jwt" },
   adapter: PrismaAdapter(prisma),
+  secret: process.env.AUTH_SECRET,
+  session: { strategy: "jwt" },
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID as string,
