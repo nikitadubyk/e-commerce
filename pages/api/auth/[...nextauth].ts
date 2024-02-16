7b337540ad6f3cdeb707d8804aab0c775973284a
Nikita Dubyk <nikitad@guardianvets.com>
2024-02-16 19:10:11 +0300

    add create stripe account for user

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,3 +1,4 @@
+import Stripe from "stripe";
 import NextAuth from "next-auth";
 import { PrismaClient } from "@prisma/client";
 import GitHubProvider from "next-auth/providers/github";
@@ -6,14 +7,33 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 const prisma = new PrismaClient();
 
 export default NextAuth({
+  debug: true,
+  session: { strategy: "jwt" },
   adapter: PrismaAdapter(prisma),
   secret: process.env.AUTH_SECRET,
-  session: { strategy: "jwt" },
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
-  debug: true,
+  events: {
+    createUser: async ({ user }) => {
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+        apiVersion: "2022-11-15",
+      });
+
+      if (user.name && user.email) {
+        const customer = await stripe.customers.create({
+          name: user.name,
+          email: user.email,
+        });
+
+        await prisma.user.update({
+          where: { id: user.id },
+          data: { stripeCustomerId: customer.id },
+        });
+      }
+    },
+  },
 });
